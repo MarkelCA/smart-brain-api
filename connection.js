4b21cc40623c11216e1c450a9f64e88958c79c1a
= <markel@bikuma.com>
2022-02-19 13:09:02 +0100

    login + register finished

--- connection.js
+++ connection.js
@@ -3,40 +3,37 @@ import dotenv from 'dotenv'
 import User from './schemas/User.js'
 dotenv.config()
 
-const { USER, PASSWORD, DB_NAME, DB_HOST } = process.env
-const url = `mongodb+srv://${USER}:${PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
+const { FULL_DB_URL, DB_USER, PASSWORD, DB_NAME, DB_HOST } = process.env
+const url = FULL_DB_URL ? FULL_DB_URL : `mongodb+srv://${DB_USER}:${PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
 
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
 export const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-export const printErrors = (err) => {
+export const getErrors = (err) => {
     let strMessage = "Error:\n"
 
     for(const errMessage in err.errors)
         strMessage += " - " + err.errors[errMessage].message + "\n"
-    console.log(strMessage)
+
+    return(strMessage)
 }
 
 export const findUser = async (condition) => {
-     return await User.find(condition).exec()
+     return await User.findOne(condition).exec()
 }
-export const newUser = async(user) => {
+export const insertUser = async(user) => {
     const newuser = new User(user)
-    let ret = null
-    await newuser.save((err, doc) => {
-        if(err)
-            printErrors(err)
-        else {
-            console.log('doc')
-            console.log(doc)
-            ret = doc
-            console.log(ret)
-        }
 
-    })
+    const insertedUser = await new Promise((resolve, reject) => {
+        newuser.save((err, res) => {
+            if(err) reject( getErrors(err) )
+            else resolve(res)
+        })
+    }) 
 
-    return ret
+    return insertedUser
+
 }
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 // Bcrypt config
 import bcrypt from 'bcrypt'
-import { db, findUser, newUser } from './connection.js'
+import { db, findUser, insertUser } from './connection.js'
 
 const saltRounds = 10;
 const myPlaintextPassword = 's0/\/\P4$$w0rD';
@@ -15,80 +15,41 @@ db.once('open', async function() {
     console.log('Connected to database')
 });
 
-//const user = await findUser({email : "markel@gmail.com"})
-(async() => {
-    const user = await newUser({name : "Bob", password : 1234, email : "iaionewMarkefjlj@jgmail.com"}).then((user) => {
-        console.log('hii')
-        console.log(user)
-    })
-
-})()
-
-//
 
 // Middleware
 app.use(bodyParser.json())
 app.use(cors())
 
 // Routes
-const database = {
-    users : [
-        {
-            id       : 123,
-            email    : "john@email.com",
-            name     : 'John',
-            password : '$2b$10$8wX8Kjg2TdTVlTYODCG9Ou6uxQsI2sO6ZRakqKwMwR/DgIpwVVvC6',
-            entries  : 0,
-            joined   : new Date()
-        },
-        {
-            id       : 124,
-            name     : 'Sally',
-            email    : "sally@email.com",
-            password : 'bananas',
-            entries  : 3,
-            joined   : new Date()
-        },
-        
-    ]
-}
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params
-    const user = database.users.filter(user => user.id == id)[0]
-    res.json(user ?? "nothing")
-
-})
-
-app.get('/', (req, res) => {
-    res.json(database.users)
-})
-
 app.post('/register', async (req, res) => {
     const { email, password, name} = req.body
 
-    console.log(password)
+    const hash = await bcrypt.hash(password, saltRounds)
 
-    //const hash = await bcrypt.hash(password, saltRounds)
+    const user = {
+        name     : name,
+        email    : email,
+        password : hash,
+        entries  : 0,
+        joined   : new Date()
+    } 
 
-    database.users.push({
-            id       : 125,
-            name     : name,
-            email    : email,
-            password : password,
-            entries  : 0,
-            joined   : new Date()
-        })
-    res.json('created new user')
+    const existingUser = await findUser({email : email})
+    if(existingUser) {
+        res.json(false)
+        return
+    }
+
+    await insertUser(user).then((user) => {
+        res.json(user)
+    }).catch(console.log)
     
 })
 
-app.put('/image', (req, res) => {
-    const { id } = req.body
-    const user = database.users.filter(user => user.id == id)[0]
-    console.log(user)
-    if(user) {
-        console.log(user)
+app.put('/image', async (req, res) => {
+    const { email } = req.body
+    const user = await findUser({ email : email})
+        if(user) {
         user.entries++
         res.json(user)
     }
@@ -97,10 +58,9 @@ app.put('/image', (req, res) => {
 
 })
 
-app.post('/signin', (req, res) => {
+app.post('/signin', async (req, res) => {
     const { email: sendedEmail, password : sendedPassword} = req.body
-    const user = database.users.find(user => user.email == sendedEmail)
-
+    const user = await findUser({email : sendedEmail})
     if(!user) 
         res.json(null)
     else
